1f6a7fbc36a964c44482d3a0fbe12a6fd597b918
Linga427 <906999536@qq.com>
2024-10-24 18:45:48 +0800

    vue3

---
+++ vite.config.js
@@ -0,0 +1,51 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver({ importStyle: 'sass' }),
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+          @use "@/styles/var.scss" as *;
+        `,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist', // 打包输出目录
+    base: './', // 修改为你的项目基础路径（如果有）
+    minify: 'terser', // 使用 terser 进行压缩
+    chunkSizeWarningLimit: 600, // 警告大小限制
+    sourcemap: true, //是否构建source map 文件
+    terserOptions: {
+      // 生产环境移除console
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+
+});
